68fd9051a284ed56b7d094d5f5d9787b47ef89d1
Jane Moroz <Moroz.jane@yahoo.com>
2022-06-24 16:51:10 +0300

    Added global styles, images

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,39 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "My Portfolio",
+    description: "Jane Moroz's Portfolio",
+    author: "@janemoroz",
+  },
+  plugins: [
+    { resolve: `gatsby-plugin-styled-components` },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Montserrat",
+              variants: ["300", "400", "500"],
+            },
+            {
+              family: "Fascinate",
+              variants: ["400"],
+            },
+          ],
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+  ],
 }
