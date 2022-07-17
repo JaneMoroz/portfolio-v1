0018e74c2d11bc4b84839480fd5f7dc0719ce2ea
Jane Moroz <Moroz.jane@yahoo.com>
2022-07-17 15:02:10 +0300

    Added error page localization

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,25 +3,37 @@ import Layout from "../components/Layout"
 import Wrapper from "../assets/wrappers/404"
 import { Seo } from "../components"
 import { cat } from "../assets/images"
-import { Link } from "gatsby"
+import SimpleLocalize from "../utils/SimpleLocalize"
+import LinkTranslated from "../utils/LinkTranslated"
+import { FormattedMessage } from "react-intl"
 
-const Error = () => {
+const Error = props => {
   return (
-    <Layout>
-      <Seo title={"Error Page"} />
-      <Wrapper>
-        <section>
-          <span>404</span>
-          <h1>Page not found!</h1>
-          <Link to="/" className="btn btn-outlined">
-            back to home page
-          </Link>
-        </section>
-        <div className="background">
-          <img src={cat} alt="cat" />
-        </div>
-      </Wrapper>
-    </Layout>
+    <SimpleLocalize {...props}>
+      <Layout>
+        <Seo title={"Error Page"} />
+        <Wrapper>
+          <section>
+            <span>404</span>
+            <h1>
+              <FormattedMessage
+                id="not-found"
+                defaultMessage="Page not found!"
+              />
+            </h1>
+            <LinkTranslated href="/" className="btn btn-outlined">
+              <FormattedMessage
+                id="back-to-home-btn"
+                defaultMessage="back to home page"
+              />
+            </LinkTranslated>
+          </section>
+          <div className="background">
+            <img src={cat} alt="cat" />
+          </div>
+        </Wrapper>
+      </Layout>
+    </SimpleLocalize>
   )
 }
 
