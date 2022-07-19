a3e0f6b7e37877da627843f624d0ffdecca101d0
Jane Moroz <Moroz.jane@yahoo.com>
2022-07-19 13:54:17 +0300

    Fixed possible local storage bug

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,7 +1,11 @@
 // Get theme from the local storage
 export const getStorageTheme = () => {
   let theme = "light-theme"
-  if (localStorage.getItem("theme")) {
+  if (
+    localStorage.getItem("theme") &&
+    (localStorage.getItem("theme") === "light-theme" ||
+      localStorage.getItem("theme") === "dark-theme")
+  ) {
     theme = localStorage.getItem("theme")
   }
   return theme
